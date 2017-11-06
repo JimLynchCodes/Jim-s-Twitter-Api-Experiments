15bd86e8246bde0eb2928257906c8de12e3d0a21
JimTheMan <mrdotjim@gmail.com>
2017-11-06 00:29:56 -0500

    deleted file

--- js_search/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    consumer_key:         'POg6uj8z59LVc0wU4WXfmkKSz',
-    consumer_secret:      'hwvDqPAoLMPhLCChfDoR0eHLh5GhjvlNM0SwYhJk3nnuKh9F9M',
-    access_token:         '3930103636-jkhaz35nQHoukT08mX2L7TPI1Toklae8ajTliI0',
-    access_token_secret:  'wvRTeURAUDvBJ4JLNu1OvDWZZEcc11uqxIo0RoFc10nT2',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-};
